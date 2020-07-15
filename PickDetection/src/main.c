45ac9e01129e023d8f21114e347f67b3c139b51d
David Abraham <51338146+DAA2310@users.noreply.github.com>
2020-07-15 16:58:17 -0400

    Component Inclusion (#6)
    * before build attempt
    
    * Camera library component inclusion
    
    * HelloWorld (#7)
    
    * before build attempt
    
    * Setting up helloWorld project
    
    * before build attempt
    
    * Camera library component inclusion
    
    * camera initialization
    
    * removing stale files

--- PickDetection/src/main.c
+++ PickDetection/src/main.c
@@ -50,7 +50,8 @@
 #include "esp_wifi.h"
 #include "esp_interface.h"
 #include "esp_bt.h"
-
+#include "esp_camera.h" //
+#include "esp_log.h" //
 #if CONFIG_NIMBLE_ENABLED == 1
     #include "esp_nimble_hci.h"
 #else
@@ -75,6 +76,52 @@
     #include "iot_ble_numericComparison.h"
 #endif
 
+/**
+ * PIXFORMAT_RGB565,    // 2BPP/RGB565
+ * PIXFORMAT_YUV422,    // 2BPP/YUV422
+ * PIXFORMAT_GRAYSCALE, // 1BPP/GRAYSCALE
+ * PIXFORMAT_JPEG,      // JPEG/COMPRESSED
+ * PIXFORMAT_RGB888,    // 3BPP/RGB888
+ */
+#define CAMERA_PIXEL_FORMAT PIXFORMAT_JPEG
+
+/*
+ * FRAMESIZE_QQVGA,    // 160x120
+ * FRAMESIZE_QQVGA2,   // 128x160
+ * FRAMESIZE_QCIF,     // 176x144
+ * FRAMESIZE_HQVGA,    // 240x176
+ * FRAMESIZE_QVGA,     // 320x240
+ * FRAMESIZE_CIF,      // 400x296
+ * FRAMESIZE_VGA,      // 640x480
+ * FRAMESIZE_SVGA,     // 800x600
+ * FRAMESIZE_XGA,      // 1024x768
+ * FRAMESIZE_SXGA,     // 1280x1024
+ * FRAMESIZE_UXGA,     // 1600x1200
+ */
+#define CAMERA_FRAME_SIZE FRAMESIZE_QVGA
+
+/* clock frequency */
+#define XCLK_FREQ       20000000
+
+/* CONFIG_CAMERA_MODEL_ESP_EYE  */
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    4
+#define SIOD_GPIO_NUM    18
+#define SIOC_GPIO_NUM    23
+
+#define Y9_GPIO_NUM      36
+#define Y8_GPIO_NUM      37
+#define Y7_GPIO_NUM      38
+#define Y6_GPIO_NUM      39
+#define Y5_GPIO_NUM      35
+#define Y4_GPIO_NUM      14
+#define Y3_GPIO_NUM      13
+#define Y2_GPIO_NUM      34
+#define VSYNC_GPIO_NUM   5
+#define HREF_GPIO_NUM    27
+#define PCLK_GPIO_NUM    25
+
 /* Logging Task Defines. */
 #define mainLOGGING_MESSAGE_QUEUE_LENGTH    ( 32 )
 #define mainLOGGING_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 4 )
@@ -82,7 +129,57 @@
 
 QueueHandle_t spp_uart_queue = NULL;
 
+static const char *TAG = "app_camera";
 
+void app_camera_init()
+{
+    /* IO13, IO14 is designed for JTAG by default,
+     * to use it as generalized input,
+     * firstly declair it as pullup input */
+    gpio_config_t conf;
+    conf.mode = GPIO_MODE_INPUT;
+    conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    conf.intr_type = GPIO_INTR_DISABLE;
+    conf.pin_bit_mask = 1LL << 13;
+    gpio_config(&conf);
+    conf.pin_bit_mask = 1LL << 14;
+    gpio_config(&conf);
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = -1;//RESET_GPIO_NUM;
+    config.xclk_freq_hz = XCLK_FREQ;
+    config.pixel_format = CAMERA_PIXEL_FORMAT;
+    config.frame_size = CAMERA_FRAME_SIZE;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+        return;
+    }
+    if (err == ESP_OK)
+        configPRINTF(("Hello again\n"));
+}
 /* Static arrays for FreeRTOS+TCP stack initialization for Ethernet network connections
  * are use are below. If you are using an Ethernet connection on your MCU device it is
  * recommended to use the FreeRTOS+TCP stack. The default values are defined in
@@ -137,6 +234,7 @@ int app_main( void )
         /* Run all demos. */
         //DEMO_RUNNER_RunDemos();
         configPRINTF(("Hello !!!!!!\n"));
+        app_camera_init();
     }
 
     /* Start the scheduler.  Initialization that requires the OS to be running,
