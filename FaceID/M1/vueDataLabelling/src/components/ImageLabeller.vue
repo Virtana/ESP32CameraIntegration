848c8ed1c0022d7583839b225dcc1880950e8987
RaeanneRamharrack <raeanne.ramharrack@my.uwi.edu>
2020-07-24 15:47:33 -0400

    bootsraping edits

--- FaceID/M1/vueDataLabelling/src/components/ImageLabeller.vue
+++ FaceID/M1/vueDataLabelling/src/components/ImageLabeller.vue
@@ -1,7 +1,89 @@
 <template>
   <div class="hello">
-    <div class="mt-3">
+    <div>
+      <b-container class="bv-example-row">
+        <b-row class="text-center">
+          <b-col>
+            <div class="px-3 py-2">
+            <b-form-group title="EMPLOYE NAME">
+              <b-form-radio-group
+                id="btn-radios-3"
+                v-model="selected"
+                v-on:input="loadNext()"
+                :options="options"
+                buttons
+                stacked
+                name="radio-btn-stacked"
+              ></b-form-radio-group>
+            </b-form-group>
+        </div>
+          </b-col>
+          <b-col cols="8">
+            <div>
+              <b-card-group>
+                <b-card header-tag="header" footer-tag="footer" >
+                  <template v-slot:header>
+                    <h6 class="mb-0">Who's this?</h6>
+                  </template>                  
+                    <!-- <h2> {{eData[(this.loc % this.images.length)].label}} </h2> -->
+                    <img id="pic" src="" fluid alt="Hard code" height="300px"/>
+                  <template v-slot:footer>
+                    <b-button v-on:click="loadPrev()" variant="primary">Prev</b-button>
+                    <!-- <em>{{selected}}</em> -->
+                    <b-button v-on:click="loadNext()" variant="primary">Next</b-button>
+                  </template>
+                </b-card>
+              </b-card-group>
+            </div>
+
+            <!-- <div class="mt-3">
+              <b-button-group>
+                <b-button variant="info" v-on:click="chooseLabel()">Check</b-button>
+                <b-button variant="info" v-on:click="loadPrev()">Prev</b-button>
+                <b-button variant="info" v-on:click="loadNext()">Next</b-button>
+              </b-button-group>
+            </div>
+            <div>
+              <div>
+                <img id="pic" src="" fluid alt="Hard code" height="300px"/>
+              </div>
+              <label>{{selected}} </label><br>
+              <input id="inputName" type="text" v-on:keyup="save()" value="this.eData[(this.loc % this.images.length)].label" v-model="name"> 
+              <div>
+                
+              </div>
+            </div> -->
+            <div>
+              <vue-csv-downloader
+                  :data="eData"
+                  :fields="fields"
+              > Download CSV File
+              </vue-csv-downloader>
+            </div>
+          </b-col>
+          <b-col>3 of 3</b-col>
+        </b-row>
+      </b-container>
+      <!-- <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button> -->
+      <!-- <b-sidebar visible id="sidebar-1" title="Choose Employee Name" shadow static>
+        <div class="px-3 py-2">
+            <b-form-group >
+              <b-form-radio-group
+                id="btn-radios-3"
+                v-model="selected"
+                v-on:input="loadNext()"
+                :options="options"
+                buttons
+                stacked
+                name="radio-btn-stacked"
+              ></b-form-radio-group>
+            </b-form-group>
+        </div>
+      </b-sidebar> -->
+    </div>
+    <!-- <div class="mt-3">
       <b-button-group>
+        <b-button variant="info" v-on:click="chooseLabel()">Check</b-button>
         <b-button variant="info" v-on:click="loadPrev()">Prev</b-button>
         <b-button variant="info" v-on:click="loadNext()">Next</b-button>
       </b-button-group>
@@ -10,8 +92,11 @@
       <div>
         <img id="pic" src="" fluid alt="Hard code" height="300px"/>
       </div>
-      <label>Enter Name: </label><br>
+      <label>{{selected}} </label><br>
       <input id="inputName" type="text" v-on:keyup="save()" value="this.eData[(this.loc % this.images.length)].label" v-model="name"> 
+      <div>
+        
+      </div>
     </div>
     <div>
       <vue-csv-downloader
@@ -19,7 +104,7 @@
           :fields="fields"
       > Download CSV File
       </vue-csv-downloader>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -38,13 +123,30 @@ export default {
   data: function () {
     return {
       images: [],
-      loc: 0,
+      loc: -1,
       name: '',
       eData: [],
       fields: ['label', 'image'],
+      labels: ['kohai', 'senpaii'],
+      options: [
+          { text: 'Captain', value: 'Captain' },
+          { text: 'Thor', value: 'Thor' },
+          { text: 'Charlie', value: 'Charlie'},
+          { text: 'Zendaya', value: 'Zendaya' },
+          { text: 'Kim K', value: 'Kim' },
+          { text: 'Michael', value: 'Michael' },
+          { text: 'Taraji', value: 'Taraji'}
+        ],
+      selected: ''
     }
   },
   methods: {
+    chooseLabel: function (){
+      console.log("SELECTEDDDDDD")
+      console.log(this.selected)
+      let temp = this.selected
+      setTimeout(function(){ console.log(temp); }, 3000);
+    },
     getFileNames : function (){
       var temp = require.context('@/assets', true, /\.jpg$/)
       this.images = temp.keys()
@@ -59,25 +161,28 @@ export default {
       console.log(this.eData)
     },
     clear: function (){
-      document.getElementById("inputName").value = ''
-      document.getElementById("inputName").placeholder = this.eData[(this.loc % this.images.length)].label
+      this.selected = "unknown"
+      // document.getElementById("inputName").value = ''
+      // document.getElementById("inputName").placeholder = this.eData[(this.loc % this.images.length)].label
     },
     loadFirst: function (){
       document.getElementById("pic").src = require('../assets/'+this.images[0])
     },
     loadNext: function (){
       this.loc = this.loc + 1
-      this.clear()
+      this.save()
       document.getElementById("pic").src = require('../assets/'+this.images[(this.loc % this.images.length)])
+      this.clear()
     },
     loadPrev: function (){
       this.loc = this.loc - 1
       console.log(this.loc)
-      this.clear()
+      // this.clear()
       document.getElementById("pic").src = require('../assets/'+this.images[(this.loc % this.images.length)])
     },
     save: function (){
-      this.eData[(this.loc % this.images.length)].label = this.name
+      // this.eData[(this.loc % this.images.length)].label = this.name
+      this.eData[(this.loc % this.images.length)].label = this.selected
       console.log(this.eData)
     }
   },
@@ -105,5 +210,9 @@ li {
 }
 a {
   color: #42b983;
+}
+/* SIDE BAR STUUF */
+#sidebar-1 {
+  position: fixed 
 }
 </style>
