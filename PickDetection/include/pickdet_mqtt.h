543effe77ab2e112400dc53d9e939ec6b46a592b
DAA2310 <davidarnoldabraham@outlook.com>
2020-08-04 15:26:12 -0400

    code view

---
+++ PickDetection/include/pickdet_mqtt.h
@@ -0,0 +1,9 @@
+/* Platform layer includes. */
+#include "platform/iot_clock.h"
+#include "platform/iot_threads.h"
+#include "iot_network_manager_private.h"
+
+/* MQTT include. */
+#include "iot_mqtt.h"
+
+int pickdet_mqtt_main();
