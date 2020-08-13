3c56f5324ab12983495a8be94a6b4207b268573f
David Abraham <51338146+DAA2310@users.noreply.github.com>
2020-08-13 10:42:56 -0400

    Code modularization w/ motion detect (#15)
    * Code modularization w/ motion detect
    
    * header refactor, main rename and task inclusion for motion detect

---
+++ PickDetection/include/pickdet_motion.h
@@ -0,0 +1,14 @@
+//change according to resolution chosen in pickdet_camera.h
+#define WIDTH 320 
+#define HEIGHT 240
+#define BLOCK_SIZE 10
+#define W (WIDTH / BLOCK_SIZE)
+#define H (HEIGHT / BLOCK_SIZE)
+#define BLOCK_DIFF_THRESHOLD 0.15
+#define IMAGE_DIFF_THRESHOLD 0.15
+
+void print_frame();
+void app_capture_still();
+bool app_motion_detect();
+void app_update_frame();
+void pickdet_motion_detect();
--- PickDetection/src/main.c
+++ PickDetection/src/main.c
@@ -31,12 +31,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* Demo includes */
-#include "aws_demo.h"
-#include "aws_dev_mode_key_provisioning.h"
-
 /* AWS System includes. */
-#include "bt_hal_manager.h"
 #include "iot_system_init.h"
 #include "iot_logging_task.h"
 
@@ -49,204 +44,26 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_interface.h"
-#include "esp_bt.h"
-#include "esp_camera.h" //
-#include "esp_log.h" //
-#if CONFIG_NIMBLE_ENABLED == 1
-    #include "esp_nimble_hci.h"
-#else
-    #include "esp_gap_ble_api.h"
-    #include "esp_bt_main.h"
-#endif
+#include "esp_log.h" 
 
-#include "driver/uart.h"
+#include "esp_camera.h" 
+#include "math.h"
+
 #include "aws_application_version.h"
-#include "tcpip_adapter.h"
 
-#include "iot_network_manager_private.h"
-
-#if BLE_ENABLED
-    #include "bt_hal_manager_adapter_ble.h"
-    #include "bt_hal_manager.h"
-    #include "bt_hal_gatt_server.h"
-
-    #include "iot_ble.h"
-    #include "iot_ble_config.h"
-    #include "iot_ble_wifi_provisioning.h"
-    #include "iot_ble_numericComparison.h"
-#endif
-
-/**
- * PIXFORMAT_RGB565,    // 2BPP/RGB565
- * PIXFORMAT_YUV422,    // 2BPP/YUV422
- * PIXFORMAT_GRAYSCALE, // 1BPP/GRAYSCALE
- * PIXFORMAT_JPEG,      // JPEG/COMPRESSED
- * PIXFORMAT_RGB888,    // 3BPP/RGB888
- */
-#define CAMERA_PIXEL_FORMAT PIXFORMAT_JPEG
-
-/*
- * FRAMESIZE_QQVGA,    // 160x120
- * FRAMESIZE_QQVGA2,   // 128x160
- * FRAMESIZE_QCIF,     // 176x144
- * FRAMESIZE_HQVGA,    // 240x176
- * FRAMESIZE_QVGA,     // 320x240
- * FRAMESIZE_CIF,      // 400x296
- * FRAMESIZE_VGA,      // 640x480
- * FRAMESIZE_SVGA,     // 800x600
- * FRAMESIZE_XGA,      // 1024x768
- * FRAMESIZE_SXGA,     // 1280x1024
- * FRAMESIZE_UXGA,     // 1600x1200
- */
-#define CAMERA_FRAME_SIZE FRAMESIZE_QVGA
-
-/* clock frequency */
-#define XCLK_FREQ       20000000
-
-/* CONFIG_CAMERA_MODEL_ESP_EYE  */
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    4
-#define SIOD_GPIO_NUM    18
-#define SIOC_GPIO_NUM    23
-
-#define Y9_GPIO_NUM      36
-#define Y8_GPIO_NUM      37
-#define Y7_GPIO_NUM      38
-#define Y6_GPIO_NUM      39
-#define Y5_GPIO_NUM      35
-#define Y4_GPIO_NUM      14
-#define Y3_GPIO_NUM      13
-#define Y2_GPIO_NUM      34
-#define VSYNC_GPIO_NUM   5
-#define HREF_GPIO_NUM    27
-#define PCLK_GPIO_NUM    25
+#include "pickdet_camera.h"
+#include "pickdet_motion.h"
 
 /* Logging Task Defines. */
 #define mainLOGGING_MESSAGE_QUEUE_LENGTH    ( 32 )
 #define mainLOGGING_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 4 )
-#define mainDEVICE_NICK_NAME                "Espressif_Demo"
 
-QueueHandle_t spp_uart_queue = NULL;
-
-static const char *TAG = "app_camera";
-
-void app_camera_init()
-{
-    /* IO13, IO14 is designed for JTAG by default,
-     * to use it as generalized input,
-     * firstly declair it as pullup input */
-    gpio_config_t conf;
-    conf.mode = GPIO_MODE_INPUT;
-    conf.pull_up_en = GPIO_PULLUP_ENABLE;
-    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    conf.intr_type = GPIO_INTR_DISABLE;
-    conf.pin_bit_mask = 1LL << 13;
-    gpio_config(&conf);
-    conf.pin_bit_mask = 1LL << 14;
-    gpio_config(&conf);
-
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = -1;//RESET_GPIO_NUM;
-    config.xclk_freq_hz = XCLK_FREQ;
-    config.pixel_format = CAMERA_PIXEL_FORMAT;
-    config.frame_size = CAMERA_FRAME_SIZE;
-    config.jpeg_quality = 10;
-    config.fb_count = 1;
-
-    // camera init
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
-        return;
-    }
-    if (err == ESP_OK)
-        configPRINTF(("Hello again\n"));
-}
-/* Static arrays for FreeRTOS+TCP stack initialization for Ethernet network connections
- * are use are below. If you are using an Ethernet connection on your MCU device it is
- * recommended to use the FreeRTOS+TCP stack. The default values are defined in
- * FreeRTOSConfig.h. */
-
-/**
- * @brief Initializes the board.
- */
-static void prvMiscInitialization( void );
-
-#if BLE_ENABLED
-/* Initializes bluetooth */
-    static esp_err_t prvBLEStackInit( void );
-    /** Helper function to teardown BLE stack. **/
-    esp_err_t xBLEStackTeardown( void );
-    static void spp_uart_init( void );
-#endif
+static const char *TAG = "STATUS";
 
 /*-----------------------------------------------------------*/
 
-/**
- * @brief Application runtime entry point.
- */
-int app_main( void )
-{
-    /* Perform any hardware initialization that does not require the RTOS to be
-     * running.  */
-
-    prvMiscInitialization();
-
-    if( SYSTEM_Init() == pdPASS )
-    {
-        /* A simple example to demonstrate key and certificate provisioning in
-         * microcontroller flash using PKCS#11 interface. This should be replaced
-         * by production ready key provisioning mechanism. */
-        vDevModeKeyProvisioning();
-
-        #if BLE_ENABLED
-            /* Initialize BLE. */
-            if( prvBLEStackInit() != ESP_OK )
-            {
-                configPRINTF( ( "Failed to initialize the bluetooth stack\n " ) );
-
-                while( 1 )
-                {
-                }
-            }
-        #else
-            ESP_ERROR_CHECK( esp_bt_controller_mem_release( ESP_BT_MODE_CLASSIC_BT ) );
-            ESP_ERROR_CHECK( esp_bt_controller_mem_release( ESP_BT_MODE_BLE ) );
-        #endif /* if BLE_ENABLED */
-        /* Run all demos. */
-        //DEMO_RUNNER_RunDemos();
-        configPRINTF(("Hello !!!!!!\n"));
-        app_camera_init();
-    }
-
-    /* Start the scheduler.  Initialization that requires the OS to be running,
-     * including the WiFi initialization, is performed in the RTOS daemon task
-     * startup hook. */
-    /* Following is taken care by initialization code in ESP IDF */
-    /* vTaskStartScheduler(); */
-    return 0;
-}
-
-/*-----------------------------------------------------------*/
 extern void vApplicationIPInit( void );
+
 static void prvMiscInitialization( void )
 {
     /* Initialize NVS */
@@ -260,182 +77,21 @@ static void prvMiscInitialization( void )
 
     ESP_ERROR_CHECK( ret );
 
-    #if BLE_ENABLED
-        NumericComparisonInit();
-        spp_uart_init();
-    #endif
-
     /* Create tasks that are not dependent on the WiFi being initialized. */
     xLoggingTaskInitialize( mainLOGGING_TASK_STACK_SIZE,
                             tskIDLE_PRIORITY + 5,
                             mainLOGGING_MESSAGE_QUEUE_LENGTH );
 
-#if AFR_ESP_LWIP
-    configPRINTF( ("Initializing lwIP TCP stack\r\n") );
-    tcpip_adapter_init();
-#else
-    configPRINTF( ("Initializing FreeRTOS TCP stack\r\n") );
-    vApplicationIPInit();
-#endif
-}
-
-/*-----------------------------------------------------------*/
-
-#if BLE_ENABLED
-
-    #if CONFIG_NIMBLE_ENABLED == 1
-        esp_err_t prvBLEStackInit( void )
-        {
-            return ESP_OK;
-        }
-
-
-        esp_err_t xBLEStackTeardown( void )
-        {
-            esp_err_t xRet;
-
-            xRet = esp_bt_controller_mem_release( ESP_BT_MODE_BLE );
-
-            return xRet;
-        }
-
-    #else /* if CONFIG_NIMBLE_ENABLED == 1 */
-
-        static esp_err_t prvBLEStackInit( void )
-        {
-            return ESP_OK;
-        }
-
-        esp_err_t xBLEStackTeardown( void )
-        {
-            esp_err_t xRet = ESP_OK;
-
-            if( esp_bluedroid_get_status() == ESP_BLUEDROID_STATUS_ENABLED )
-            {
-                xRet = esp_bluedroid_disable();
-            }
-
-            if( xRet == ESP_OK )
-            {
-                xRet = esp_bluedroid_deinit();
-            }
-
-            if( xRet == ESP_OK )
-            {
-                if( esp_bt_controller_get_status() == ESP_BT_CONTROLLER_STATUS_ENABLED )
-                {
-                    xRet = esp_bt_controller_disable();
-                }
-            }
-
-            if( xRet == ESP_OK )
-            {
-                xRet = esp_bt_controller_deinit();
-            }
-
-            if( xRet == ESP_OK )
-            {
-                xRet = esp_bt_controller_mem_release( ESP_BT_MODE_BLE );
-            }
-
-            if( xRet == ESP_OK )
-            {
-                xRet = esp_bt_controller_mem_release( ESP_BT_MODE_BTDM );
-            }
-
-            return xRet;
-        }
-    #endif /* if CONFIG_NIMBLE_ENABLED == 1 */
-#endif /* if BLE_ENABLED */
-
-/*-----------------------------------------------------------*/
-
-#if BLE_ENABLED
-    static void spp_uart_init( void )
-    {
-        uart_config_t uart_config =
-        {
-            .baud_rate           = 115200,
-            .data_bits           = UART_DATA_8_BITS,
-            .parity              = UART_PARITY_DISABLE,
-            .stop_bits           = UART_STOP_BITS_1,
-            .flow_ctrl           = UART_HW_FLOWCTRL_RTS,
-            .rx_flow_ctrl_thresh = 122,
-        };
-
-        /* Set UART parameters */
-        uart_param_config( UART_NUM_0, &uart_config );
-        /*Set UART pins */
-        uart_set_pin( UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE );
-        /*Install UART driver, and get the queue. */
-        uart_driver_install( UART_NUM_0, 4096, 8192, 10, &spp_uart_queue, 0 );
-    }
-
-/*-----------------------------------------------------------*/
-
-    BaseType_t getUserMessage( INPUTMessage_t * pxINPUTmessage,
-                               TickType_t xAuthTimeout )
-    {
-        uart_event_t xEvent;
-        BaseType_t xReturnMessage = pdFALSE;
-
-        if( xQueueReceive( spp_uart_queue, ( void * ) &xEvent, ( portTickType ) xAuthTimeout ) )
-        {
-            switch( xEvent.type )
-            {
-                /*Event of UART receiving data */
-                case UART_DATA:
-
-                    if( xEvent.size )
-                    {
-                        pxINPUTmessage->pcData = ( uint8_t * ) malloc( sizeof( uint8_t ) * xEvent.size );
-
-                        if( pxINPUTmessage->pcData != NULL )
-                        {
-                            memset( pxINPUTmessage->pcData, 0x0, xEvent.size );
-                            uart_read_bytes( UART_NUM_0, ( uint8_t * ) pxINPUTmessage->pcData, xEvent.size, portMAX_DELAY );
-                            xReturnMessage = pdTRUE;
-                        }
-                        else
-                        {
-                            configPRINTF( ( "Malloc failed in main.c\n" ) );
-                        }
-                    }
-
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        return xReturnMessage;
-    }
-#endif /* if BLE_ENABLED */
-
-/*-----------------------------------------------------------*/
-
-extern void esp_vApplicationTickHook();
-void IRAM_ATTR vApplicationTickHook()
-{
-    esp_vApplicationTickHook();
-}
-
-/*-----------------------------------------------------------*/
-extern void esp_vApplicationIdleHook();
-void vApplicationIdleHook()
-{
-    esp_vApplicationIdleHook();
-}
-
-/*-----------------------------------------------------------*/
-
-void vApplicationDaemonTaskStartupHook( void )
-{
+    #if AFR_ESP_LWIP
+        configPRINTF( ("Initializing lwIP TCP stack\r\n") );
+        tcpip_adapter_init();
+    #else
+        configPRINTF( ("Initializing FreeRTOS TCP stack\r\n") );
+        vApplicationIPInit();
+    #endif
 }
 
 #if !AFR_ESP_LWIP
-/*-----------------------------------------------------------*/
 void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
 {
     uint32_t ulIPAddress, ulNetMask, ulGatewayAddress, ulDNSServerAddress;
@@ -460,3 +116,17 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
     }
 }
 #endif
+
+/*-----------------------------------------------------------*/
+
+int app_main( void )
+{
+    prvMiscInitialization();
+
+    if( SYSTEM_Init() == pdPASS )
+    {
+        pickdet_cam_init();
+        xTaskCreate(pickdet_motion_detect, "Motion Detection",10000,NULL,1,NULL);
+    }   
+    return 0;
+}
